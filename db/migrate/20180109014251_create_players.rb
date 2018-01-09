00cbfe872cfb419116ae66aab7e2361c060aec0b
alberto <github email address>
2018-01-08 20:52:46 -0500

    Added Player model

---
+++ db/migrate/20180109014251_create_players.rb
@@ -0,0 +1,17 @@
+class CreatePlayers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :players do |t|
+      t.string :first_name
+      t.string :last_name
+      t.datetime :born
+      t.integer :height
+      t.integer :weight
+      t.string :position
+      t.string :nat_team
+      t.string :club_team
+      t.string :img_url
+
+      t.timestamps
+    end
+  end
+end
